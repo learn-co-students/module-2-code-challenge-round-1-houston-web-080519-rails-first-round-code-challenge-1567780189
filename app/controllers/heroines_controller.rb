31f6bfb5a9bf81d2554372286048022935c24b69
BarretteBanner <barrettebanner@gmail.com>
2019-09-06 11:07:04 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,26 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    Power.all.each do |power|
+      if power.id == @heroine.power_id
+        @power = power.name
+      end
+    end
+  end
+
+  def new
+    @powers = Power.all
+  end
+
+  def create
+    Heroine.create({
+      name: params[:name],
+      super_name: params[:super_name],
+      power_id: params[:power_id]
+    })
+    redirect_to "/heroines/"
+  end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,3 @@
 class Heroine < ApplicationRecord
+    has_one :power
 end
