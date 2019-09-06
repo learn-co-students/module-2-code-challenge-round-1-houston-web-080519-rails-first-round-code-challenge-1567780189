09ec1082da33ce71581ef280b2fa75a27e363eae
lfrederick2106 <lfrederick2106@gmail.com>
2019-09-06 10:44:29 -0500

    commiting everything

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,21 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+  end
+
+  def create
+    Heroine.create({
+      name: params[:name],
+      super_name: params[:super_name],
+      power_id: params[:power_id]
+    })
+    redirect_to("/heroines")
+  end
+
+  def show
+    @heroine = Heroine.find_by({ id:params[:id]})
+    @power = Power.find_by({ id:params[:id]})
+  end
 end
