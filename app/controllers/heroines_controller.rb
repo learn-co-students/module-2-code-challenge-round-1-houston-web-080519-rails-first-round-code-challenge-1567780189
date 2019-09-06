fdc0b4937265d4f6daeb856c472ddeeb3c4bf6db
bubbagump2020 <bubbagump2020@gmail.com>
2019-09-06 11:07:01 -0500

    Everything but tasks 7 & 8 are complete

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,23 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @powers = Power.all
+    @heroines = Heroine.all
+  end
+
+  def create
+    @heroine = Heroine.create({
+      name: params[:name],
+      super_name: params[:super_name],
+      power_id: params[:power_id]
+    })
+    redirect_to "/heroines"
+  end
+
 end
