69e17408112229cd8bc1159326980f6742e0a3f6
Jun Liang <illuminatemynight@gmail.com>
2019-09-06 11:02:20 -0500

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
 
   get('/powers', to: 'powers#index')
 
+  get('/powers/:id', to: 'powers#show')
+
   get('/heroines', to: 'heroines#index')
 
+  get('/heroines/:id', to: 'heroines#show')
+
+  post('/heroines', to: 'heroines#create')
+
+  get('/heroines/new', to: 'heroines#new')
 end
