3c5564e811452409c2992903f32cce7e9ff14527
Michael Evans <mmevansjr@gmail.com>
2019-09-06 09:53:44 -0500

    done

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,23 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+  
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    @power = Power.find_by_id(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
 end
