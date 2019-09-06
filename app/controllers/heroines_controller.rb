c31ba08931a990ad06cde2c7eb1d666fe406a71f
Jade Huynh <35jadeh@gmail.com>
2019-09-06 10:51:08 -0500

    working base, no validation, or sort

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,31 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @powers = Power.all
+  end
+
+  def create
+    
+    # if params[:super_name].new_record? 
+      @heroine = Heroine.create(
+      name: params[:name],
+      super_name: params[:super_name],
+      power_id: params[:power_id]
+    )
+    # end
+  
+    redirect_to "/heroines/#{@heroine.id}"
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = @heroine.power
+  end
+
+
+
 end
