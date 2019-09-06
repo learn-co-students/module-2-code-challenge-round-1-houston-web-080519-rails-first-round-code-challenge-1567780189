5bda3edb35b8dbf9144521e5903f99bce9c1c60a
Michael Evans <mmevansjr@gmail.com>
2019-09-06 10:34:12 -0500

    done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,6 +1,6 @@
 class HeroinesController < ApplicationController
   def index
-    @heroines = Heroine.all
+    @heroines = Heroine.search(params[:search])
   end
   
   def new
@@ -20,5 +20,10 @@ class HeroinesController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+  def heroines_params
+    params.require(:heroine).permit(:name, :super_name, :power_id, :search)
   end
 end
