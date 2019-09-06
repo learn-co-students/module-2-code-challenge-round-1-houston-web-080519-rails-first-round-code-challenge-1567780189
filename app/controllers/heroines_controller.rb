34f382f8363a445d434051582ecbe7ea75d6f1e6
Chase Smith <chases832@gmail.com>
2019-09-06 12:53:53 -0500

    YEA

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,24 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    render(:index)
   end
+  
+  def create
+    Heroine.create({
+      name: params[:name],
+      super_name: params[:super_name],
+      power_id: params[:power_id]
+    })
+    
+    redirect_to "/heroines"
+  end 
+  def new
+    @powers = Power.all
+    
+  end 
+  def show
+    @heroine = heroine.find_by({ id: params[:id]})
+    render(:show)
+  end 
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,7 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+  def show
+    @power =Power.find_by({id:params[:power_id]})
+  end 
 end
